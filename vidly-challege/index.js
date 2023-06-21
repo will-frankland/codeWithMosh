28b1e659065e453b613a598df188e6eb4ad96f9e
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-06-21 10:30:55 -0400

    complete basic express functions for put, delete, post and get

--- vidly-challege/index.js
+++ vidly-challege/index.js
@@ -1,15 +1,15 @@
-const Joi = require('joi');
-const express = require('express');
+const Joi = require("joi");
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 
 const genres = [
-  { id: 1, name: 'Thriller' },
-  { id: 2, name: 'Horror' },
-  { id: 3, name: 'Action' },
-  { id: 4, name: 'Comedy' },
-  { id: 5, name: 'Drama' },
+  { id: 1, name: "Thriller" },
+  { id: 2, name: "Horror" },
+  { id: 3, name: "Action" },
+  { id: 4, name: "Comedy" },
+  { id: 5, name: "Drama" },
 ];
 
 function validateGenre(genere) {
@@ -24,21 +24,21 @@ const PORT = process.env.PORT || 3004;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-app.get('/', (req, res) => {
-  res.send('Vidly app');
+app.get("/", (req, res) => {
+  res.send("Vidly app");
 });
 
-app.get('/api/genres', (req, res) => {
+app.get("/api/genres", (req, res) => {
   res.send(genres);
 });
 
-app.get('/api/genres/:id', (req, res) => {
+app.get("/api/genres/:id", (req, res) => {
   const genre = genres.find((g) => g.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('Genre not found.');
+  if (!genre) return res.status(404).send("Genre not found.");
   res.send(genre);
 });
 
-app.post('/api/genres', (req, res) => {
+app.post("/api/genres", (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -50,6 +50,23 @@ app.post('/api/genres', (req, res) => {
   res.send(genres);
 });
 
-app.put('/api/genres/:id', (req, res) => {
-  
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("Genre not found.");
+
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("Genre not found.");
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
 });
