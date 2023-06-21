12a787e42c03d52fbd0bafb23bb19fabccbf93b5
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-06-21 14:48:37 -0400

    custom middleware function including urlencoded and static

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,8 +1,14 @@
 const Joi = require("joi");
+const logger = require('./logger');
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(logger);
+
+
 
 const courses = [
   { id: 1, name: "Maths" },
