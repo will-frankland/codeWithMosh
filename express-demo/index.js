ba8940728f4a1a3b05184fcd9d56ea91c887d169
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-06-21 16:03:03 -0400

    prod/env testing parameters

--- express-demo/index.js
+++ express-demo/index.js
@@ -5,14 +5,16 @@ const logger = require('./logger');
 const express = require("express");
 const app = express();
 
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-console.log(`app: ${app.get('env')}`);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
 app.use(morgan('tiny'));
+
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  console.log('Morgan enabled...');
+}
 
 app.use(logger);
 
