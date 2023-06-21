f78cd6483b1f516a206b191a0a775d1f1db360e6
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-06-21 16:41:26 -0400

    pug intro

--- express-demo/index.js
+++ express-demo/index.js
@@ -7,6 +7,9 @@ const logger = require('./logger');
 const express = require("express");
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -36,7 +39,7 @@ const courses = [
 const PORT = process.env.PORT || 3003;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render('index', { title: 'My Express App', message: "Hello" });
 });
 
 app.get("/api/courses", (req, res) => {
